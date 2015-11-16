2644bda2c0c9f37af6a64f01fcbe7a5c3df2f27d
Piotr Gawlowicz <gawlowicz@tu-berlin.de>
2015-11-16 17:43:21 +0100

    simplification with poller

--- wishful_agent/agent.py
+++ wishful_agent/agent.py
@@ -3,7 +3,6 @@ import time
 import yaml
 from driver import *
 import gevent
-from gevent.queue import Queue
 import zmq.green as zmq
 
 class Agent(object):
@@ -13,10 +12,10 @@ class Agent(object):
         self.log.debug("Controller: {0}".format(controller))
         self.config = None
         self.driver_port = 5000
+
+        self.poller = zmq.Poller()
         pass
 
-    downlinkMgsQueue = Queue() # contains msg from global controller
-    uplinkMsgQueue = Queue()  # contains msg from drivers
     drivers = {}
     driver_groups = {}
 
@@ -39,15 +38,14 @@ class Agent(object):
                         name=driver_name,
                         path=driver_parameters['path'],
                         args=driver_parameters['args'],
-                        port=self.driver_port,
-                        queue=self.uplinkMsgQueue
+                        port=self.driver_port
                 )
             )
         pass
 
 
-    def exec_driver(self, name, path, args, port, queue):
-        new_driver = Driver(name, path, args, port, queue)
+    def exec_driver(self, name, path, args, port):
+        new_driver = Driver(name, path, args, port)
         return new_driver
 
     def add_driver(self, message_types, driver):
@@ -59,6 +57,9 @@ class Agent(object):
                 self.driver_groups[message_type].append(driver.name)
             else:
                 self.driver_groups[message_type] = [driver.name]
+
+        #register driver socket in poller
+        self.poller.register(driver.socket, zmq.POLLIN)
         pass
 
     def send_msg_to_driver(self, driver_name, msg):
@@ -72,38 +73,27 @@ class Agent(object):
         pass
 
     def process_msg_from_drivers(self):
+        # Work on requests from both controller and drivers
         while True:
-            msg = self.uplinkMsgQueue.get()
-            self.log.debug("Agent received message from driver: {0}".format(msg))
-            #TODO: send response to controller
-            self.log.debug("Agent sends message to Controller: {0}".format(msg))
+            socks = dict(self.poller.poll())
 
+            for name, driver in self.drivers.iteritems():
+                if driver.socket in socks and socks[driver.socket] == zmq.POLLIN:
+                    msg = driver.socket.recv()
+                    self.log.debug("Agent received message: {0} from driver: {1}".format(msg, name))
+                    #TODO: send response to controller
+                    self.log.debug("Agent sends message to Controller: {0}".format(msg))
 
-    def receive_mgs_from_controller(self):
-        #TODO: connect with controller, create context, socket,etc.......
-
-        #receive mgs
-        while True:
-                self.log.debug("NEW ITERATION")
-                #msg = self.socket.recv()
-                #self.downlinkMgsQueue.put(msg)
-                msg = "SET_CHANNEL"
-                self.log.debug("Agent received message: {0} from controller, put it into downlinkMsgQueued".format(msg))
-                self.downlinkMgsQueue.put(msg)
-                gevent.sleep(3)
-
-                msg = "START_SERVER"
-                self.log.debug("Agent received message: {0} from controller, put it into downlinkMsgQueued".format(msg))
-                self.downlinkMgsQueue.put(msg)
-                gevent.sleep(3)
-
-        pass
 
     def process_mgs_from_contoller(self):
-        while True:
-            msg = self.downlinkMgsQueue.get()
+        #TODO: connect with controller, create context, socket,etc.......
 
-            self.log.debug("Agent gets message : {0} from Downlink Msg Queue".format(msg))
+        while True:
+            #receive mgs
+            self.log.debug("NEW ITERATION")
+            #msg = self.socket.recv()
+            msg = "SET_CHANNEL"
+            self.log.debug("Agent received message: {0} from controller and sends it to driver".format(msg))
 
             #TODO: get msg type
             msgType = msg;
@@ -118,31 +108,19 @@ class Agent(object):
                 self.log.debug("Message Type {0} not supported".format(msgType))
                 #self.send_msg_to_driver_group("all", "msg for all drivers")
 
-
-    def schedule_send_msg_to_driver(self, driver_name, msg):
-        while True:
-            self.drivers[driver_name].send_msg_to_driver(msg)
-            gevent.sleep(2)
-        pass
-
+            gevent.sleep(3)
 
     def run(self):
         self.log.debug("Agent starting".format())
         try:
             jobs_to_join = []
 
-            #communication with drivers
-            for name, driver in self.drivers.iteritems():
-                jobs_to_join.append(
-                    gevent.spawn(driver.receive_msg_from_driver)
-                )
-
             #communication with controller
-            jobs_to_join.append(gevent.spawn(self.receive_mgs_from_controller))
-            jobs_to_join.append(gevent.spawn(self.process_mgs_from_contoller))
-
             #dummy mockup function which schedule SET_CHANNEL msg every 3 seconds,
             #to simulation of communication with controller
+            jobs_to_join.append(gevent.spawn(self.process_mgs_from_contoller))
+
+            #communication with drivers
             jobs_to_join.append(gevent.spawn(self.process_msg_from_drivers))
 
             gevent.joinall(jobs_to_join)
--- wishful_agent/driver.py
+++ wishful_agent/driver.py
@@ -5,14 +5,13 @@ import gevent
 from gevent.queue import Queue
 
 class Driver(object):
-    def __init__(self, name, path, args, port, queue):
+    def __init__(self, name, path, args, port):
         self.log = logging.getLogger("{module}.{name}".format(
             module=self.__class__.__module__, name=self.__class__.__name__))
         self.name = name
         self.path = path
         self.args = args
         self.port = port
-        self.uplinkMsgQueue = queue
 
         self.context = zmq.Context()
         self.socket = self.context.socket(zmq.PAIR)
@@ -32,12 +31,6 @@ class Driver(object):
         cmd.extend(filter(None, [self.args]))
         self.pid = subprocess.Popen(cmd)
         self.log.debug("Driver: {0}, with args: {1}, PID: {2} started".format(self.name, self.args, self.pid.pid))
-
-    def receive_msg_from_driver(self):
-         while True:
-                msg = self.socket.recv()
-                self.log.debug("Driver: {0} received msg: {1} and put it on Uplink Msg Queue".format(self.name, msg))
-                self.uplinkMsgQueue.put(msg)
 
     def send_msg_to_driver(self,msg):
         self.log.debug("Driver: {0} sends msg: {1}".format(self.name, msg))
