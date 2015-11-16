2a8b4db060163bf197b8bb0272002823ba1e7bb2
Piotr Gawlowicz <gawlowicz@tu-berlin.de>
2015-11-16 18:10:26 +0100

    simplifications

--- wishful_agent/agent.py
+++ wishful_agent/agent.py
@@ -14,7 +14,20 @@ class Agent(object):
         self.driver_port = 5000
 
         self.poller = zmq.Poller()
-        pass
+
+        self.context = zmq.Context.instance()
+        self.socket_sub = self.context.socket(zmq.SUB) # for commands from controller
+        self.socket_pair = self.context.socket(zmq.PAIR) # for setup of communication between controller
+
+        #TO BE REMOVED USED ONLY FOR SIMULATION --- START
+        self.sock_server = self.context.socket(zmq.PAIR)
+        self.sock_server.bind("tcp://127.0.0.1:8989")
+        self.socket_pair.connect("tcp://127.0.0.1:8989")
+        #TO BE REMOVED USED ONLY FOR SIMULATION --- END
+
+        #register driver socket in poller
+        self.poller.register(self.socket_sub, zmq.POLLIN)
+        self.poller.register(self.socket_pair, zmq.POLLIN)
 
     drivers = {}
     driver_groups = {}
@@ -72,56 +85,66 @@ class Agent(object):
             self.send_msg_to_driver(driver_name, msg)
         pass
 
-    def process_msg_from_drivers(self):
+    def process_msgs(self):
         # Work on requests from both controller and drivers
         while True:
             socks = dict(self.poller.poll())
 
+            originator = None
             for name, driver in self.drivers.iteritems():
                 if driver.socket in socks and socks[driver.socket] == zmq.POLLIN:
+                    originator = name
                     msg = driver.socket.recv()
                     self.log.debug("Agent received message: {0} from driver: {1}".format(msg, name))
                     #TODO: send response to controller
                     self.log.debug("Agent sends message to Controller: {0}".format(msg))
 
+            if self.socket_pair in socks and socks[self.socket_pair] == zmq.POLLIN:
+                originator = "contoller"
+                msg = self.socket_pair.recv()
+                self.log.debug("Agent received message: {0} from controller using PAIR".format(msg))
 
-    def process_mgs_from_contoller(self):
-        #TODO: connect with controller, create context, socket,etc.......
-
-        while True:
-            #receive mgs
-            self.log.debug("NEW ITERATION")
-            #msg = self.socket.recv()
-            msg = "SET_CHANNEL"
-            self.log.debug("Agent received message: {0} from controller and sends it to driver".format(msg))
+            if self.socket_sub in socks and socks[self.socket_sub] == zmq.POLLIN:
+                originator = "contoller"
+                msg = self.socket_sub.recv()
+                self.log.debug("Agent received message: {0} from controller using SUB".format(msg))
 
             #TODO: get msg type
             msgType = msg;
-            if msgType == "SET_CHANNEL":
-                #self.send_msg_to_driver("ath9k_driver", "msg for ath9k_driver : SET CHANNEL")
-                #self.send_msg_to_driver_group("radio", "msg for radio drivers : SET CHANNEL")
-                self.send_msg_to_driver_group("wifi", msg)
-            elif msgType == "START_SERVER":
-                #self.send_msg_to_driver("iperf_driver", "msg for iperf_driver : START IPERF")
-                self.send_msg_to_driver_group("performance_test", msg)
+            if originator == "contoller":
+                self.log.debug("Agent sends message: {0} to driver".format(msg))
+                if msgType == "SET_CHANNEL":
+                    #self.send_msg_to_driver("ath9k_driver", "msg for ath9k_driver : SET CHANNEL")
+                    #self.send_msg_to_driver_group("radio", "msg for radio drivers : SET CHANNEL")
+                    self.send_msg_to_driver_group("wifi", msg)
+                elif msgType == "START_SERVER":
+                    #self.send_msg_to_driver("iperf_driver", "msg for iperf_driver : START IPERF")
+                    self.send_msg_to_driver_group("performance_test", msg)
+                else:
+                    self.log.debug("Message Type {0} not supported".format(msgType))
+                    #self.send_msg_to_driver_group("all", "msg for all drivers")
             else:
-                self.log.debug("Message Type {0} not supported".format(msgType))
-                #self.send_msg_to_driver_group("all", "msg for all drivers")
+                pass
 
+
+    def simulate_contoller(self):
+        while True:
+            self.log.debug("NEW ITERATION")
+            msg = "SET_CHANNEL"
+            self.sock_server.send(msg)
             gevent.sleep(3)
+
 
     def run(self):
         self.log.debug("Agent starting".format())
         try:
             jobs_to_join = []
 
-            #communication with controller
-            #dummy mockup function which schedule SET_CHANNEL msg every 3 seconds,
-            #to simulation of communication with controller
-            jobs_to_join.append(gevent.spawn(self.process_mgs_from_contoller))
+            jobs_to_join.append(gevent.spawn(self.process_msgs))
 
-            #communication with drivers
-            jobs_to_join.append(gevent.spawn(self.process_msg_from_drivers))
+            #dummy mockup function which schedule SET_CHANNEL msg every 3 seconds,
+            #to simulate communication with controller TO BE REMOVED
+            jobs_to_join.append(gevent.spawn(self.simulate_contoller))
 
             gevent.joinall(jobs_to_join)
 
