0df79daa3ecbd9ec8df36a8755c44fabc4eb7dad
Piotr Gawlowicz <gawlowicz@tu-berlin.de>
2016-08-23 13:52:18 +0200

    wishful-agent as bin

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup, find_packages
 
+
 def readme():
     with open('README.md') as f:
         return f.read()
@@ -8,6 +9,7 @@ setup(
     name='wishful_agent',
     version='0.1.0',
     packages=find_packages(),
+    scripts=['wishful_agent/bin/wishful-agent'],
     url='http://www.wishful-project.eu/software',
     license='',
     author='Piotr Gawlowicz, Mikolaj Chwalisz',
@@ -15,5 +17,5 @@ setup(
     description='WiSHFUL Agent Implementation Framework',
     long_description='Implementation of a wireless agent using the unified programming interfaces (UPIs) of the Wishful project.',
     keywords='wireless control',
-    install_requires=['pyzmq', 'apscheduler', 'dill']
+    install_requires=['pyzmq', 'apscheduler', 'dill'],
 )
