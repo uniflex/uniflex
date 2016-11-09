129f4a47a5a2e65ffe5110eb20e8a64462425a50
Piotr Gawlowicz <gawlowicz@tu-berlin.de>
2016-11-09 12:26:52 +0100

    add author info

--- uniflex/core/timer.py
+++ uniflex/core/timer.py
@@ -1,5 +1,10 @@
 import threading
 
+__author__ = "Piotr Gawlowicz"
+__copyright__ = "Copyright (c) 2015, Technische Universitat Berlin"
+__version__ = "0.1.0"
+__email__ = "gawlowicz@tkn.tu-berlin.de"
+
 
 class Timer(object):
     def __init__(self, handler_):
